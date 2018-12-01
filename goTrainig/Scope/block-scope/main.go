0e8717df0788f25123835117cfa8cca3d3dc3c57
Neculaviovici <mocanu.robert.n@gmai.com>
2018-12-01 09:37:08 -0500

    block scope

--- goTrainig/Scope/block-scope/main.go
+++ goTrainig/Scope/block-scope/main.go
@@ -6,15 +6,25 @@ import (
 
 func main() {
 	// block level scope
-	// uncomment, dont work
+	// uncomment, doesn't work
 	// fmt.Println(y)
 	y := 22
 	fmt.Println(y)
+
+	shadow := shadow(10)
+	//shadow is the result not the function
+	// don't do these, bad coding practice to shadow the variable
+	fmt.Println(shadow)
 }
 
 func foo() {
-	// uncomment, dont work
+	// uncomment, doesn't work
 	// fmt.Println(y)
 	t := 1
 	fmt.Print(t)
+}
+
+// bad practice
+func shadow(z int) int {
+	return 28 + z
 }
